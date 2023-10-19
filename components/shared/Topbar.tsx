d8dc4c04c825bdacf2dd4c48fcf1f1edff8478ef
Vineet Karwa <vineetkarwa123@gmail.com>
2023-10-19 20:13:22 +0530

    Feat: Onboarding

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { dark } from "@clerk/themes"
+import { useRouter } from 'next/navigation'
 
 const Topbar = () => {
+    const router = useRouter();
+
     return (
         <nav className="topbar">
             <Link href="/" className='flex items-center gap-4'>
@@ -14,7 +19,7 @@ const Topbar = () => {
             <div className="flex items-center gap-1">
                 <div className='block md:hidden'>
                     <SignedIn>
-                        <SignOutButton>
+                        <SignOutButton signOutCallback={() => router.push('/sign-in')}>
                             <div className="flex cursor-point">
                                 <Image src="/assets/logout.svg" alt='logout' width={24} height={24} />
                             </div>
