92c1d83632accd0df0c238b28e014be436aa965c
Vineet Karwa <vineetkarwa123@gmail.com>
2023-10-21 15:05:44 +0530

    Toast added ,Indentation Fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,26 +1,36 @@
-import '../globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-const inter = Inter({ subsets: ['latin'] })
+import "../globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: 'ConnectThreads',
-    description: 'ConnectThreads is a Twitter-like social app for threaded conversations, private communication, and media sharing.',
+  title: 'ConnectThreads',
+  description: 'ConnectThreads is a Twitter-like social app for threaded conversations, private communication, and media sharing.',
 }
 
-export default function RootLayout({ children, }: { children: React.ReactNode }) {
-    return (
-        <ClerkProvider>
-            <html lang="en">
-                <body className={`${inter.className} bg-dark-1`}>
-                    <div className='w-full flex justify-center items-center min-h-screen'>
-                        {children}
-                    </div>
-                </body>
-            </html>
-        </ClerkProvider>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className='w-full flex justify-center items-center min-h-screen'>
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
